0e0bade8ccaa9b81a23d0760981304a3d6d9e454
sosmrraziel <lbt_alh@hotmail.fr>
2024-08-10 20:38:20 +0100

    create db

--- Eventify.sql
+++ Eventify.sql
@@ -1,7 +1,7 @@
 -- prepares a MySQL server for the project
 
 CREATE DATABASE IF NOT EXISTS Eventify;
-CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY 'eventifypass';
+CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY 'rootroot';
 GRANT ALL PRIVILEGES ON `Eventify`.* TO 'root'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'root'@'localhost';
 FLUSH PRIVILEGES;
