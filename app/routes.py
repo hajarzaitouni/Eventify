4bc975067db86419dfc254af3eb0990f80e95161
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-08-10 18:58:56 +0100

    add Bootstarp V4 CSS and js to static Folder and push home template first version

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 from flask import render_template, request, redirect
 from app import app
-from app import db
+
 
 @app.route('/')
 @app.route('/home')
 def home():
     """ Home page route. """
-    return 'Hello World!'
+    return render_template('home.html')
