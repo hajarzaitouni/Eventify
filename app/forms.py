b9bf49a8cd9606c9007f7173ab2101d6aa31a6c1
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-08-16 16:57:10 +0100

    add confirm password attribute to Registartionforms class and update Register modal

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from app.models import User
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, DateTimeLocalField
-from wtforms.validators import DataRequired, Email, ValidationError, Length
+from wtforms.validators import DataRequired, Email, ValidationError, EqualTo
 from flask_wtf.file import FileField, FileAllowed
 
 
@@ -19,6 +19,9 @@ class RegisterForm(FlaskForm):
     last_name = StringField('Last Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(),
+                                                 EqualTo('password', message='Passwords must match')])
     submit = SubmitField('Register')
 
     def validate_username(self, username):
